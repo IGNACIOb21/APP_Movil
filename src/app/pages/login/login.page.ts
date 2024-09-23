0541eb697e9767b8661271382049af84b24c8e86
IGNACIOb21 <ig.aguilaruocuc.cl>
2024-09-22 23:10:02 -0300

    semi arreglo del login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Import Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  message!: string;
+  username!: string;
+  password!: string;
 
-  constructor() { }
+  constructor(private router: Router) {} // Inject Router
 
-  ngOnInit() {
+  validateLogin(){
+    if(this.username ==='admin'&& this.password ==='12345') {
+        this.router.navigate(['/main-page']);
+    }else{
+      this.message = 'login con error';
+    }
   }
 
+
+  navigateToRegister() {
+    this.router.navigate(['/registro']); // Ensure this route exists in your app
+  }
 }
+
